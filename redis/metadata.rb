d2f34795a82363adcad848cffaf8e01181637ffc
Oliver Moser <omoser@ombook.fritz.box>
2016-03-11 16:05:07 +0100

    initial redis template

---
+++ redis/metadata.rb
@@ -0,0 +1,7 @@
+name 'redis'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures redis'
+long_description 'Installs/Configures redis'
+version '0.1.0'
